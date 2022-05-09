49eaee74f620c020268222c0275c80c8dc674760
Klayton Smith II <motoklay@gmail.com>
2022-05-09 09:17:26 +0000

    gitignore fix

--- CMakeFiles/Purple.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Purple.
-# This may be replaced when dependencies are built.
--- CMakeFiles/Purple.dir/tokenizer.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Purple.dir/tokenizer.c.o: /home/ae/Purple-Lang/tokenizer.c \
- /usr/include/stdc-predef.h /home/ae/Purple-Lang/tokenizer.h
